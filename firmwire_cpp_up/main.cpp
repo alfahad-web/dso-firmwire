5edc70f8b61cbb3c235e5536515cb4311db2f905
Al Fahad <fahad.khulna.bgpsk@gmail.com>
2025-06-16 22:31:21 +0600

    dirty code, but pixel wise update enabled

--- firmwire_cpp_up/main.cpp
+++ firmwire_cpp_up/main.cpp
@@ -1,139 +1,95 @@
 #include <windows.h>
-#include <vector>
-#include <queue>
-#include "ui_layers/0_background/bg.h"
-#include "ui_layers/1_graphing/graphing.h"
-#include "ui_layers/2_axis/axis.h"
-#include "ui_layers/3_axis_annotations/axis_annotations.h"
-#include "ui_layers/4_wave_reading/wave_reading.h"
-#include "ui_layers/5_annotations/annotations.h"
-#include "global/global.h"
 #include <thread>
-#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
+#define WIDTH 854
+#define HEIGHT 480
+#define WM_UPDATE_PIXELS (WM_USER + 1)
+
+struct PixelUpdate {
+    int x, y;
+    COLORREF color;
+};
+
+struct PixelUpdateArray {
+    PixelUpdate* pixels;
+    int count;
+};
+
+HWND g_hwnd = nullptr;
+
+// 👇 Call this function to update any number of pixels asynchronously
+void updatePixels(const std::vector<PixelUpdate>& pixels) {
+    // Launch a short-lived thread
+    std::thread([pixels]() {
+        PixelUpdateArray* arr = new PixelUpdateArray;
+        arr->count = pixels.size();
+        arr->pixels = new PixelUpdate[arr->count];
+        std::copy(pixels.begin(), pixels.end(), arr->pixels);
+
+        // Send message to main window to update
+        PostMessage(g_hwnd, WM_UPDATE_PIXELS, 0, reinterpret_cast<LPARAM>(arr));
+    }).detach();
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
-        case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
 
-            for (int y = 0; y < HEIGHT; y++) {
-                for (int x = 0; x < WIDTH; x++) {
-                    int pixelValue = abs(display_buffer[y][x][5]);
-                    if(pixelValue == 0) {
-                        pixelValue = DEFAULT_BG_COLOR;
-                    }
-                    int r = (pixelValue >> 16) & 0xFF;
-                    int g = (pixelValue >> 8) & 0xFF;
-                    int b = pixelValue & 0xFF;
-                    SetPixel(hdc, x, y, RGB(r, g, b));
-                }
+        case WM_UPDATE_PIXELS: {
+            PixelUpdateArray* arr = reinterpret_cast<PixelUpdateArray*>(lParam);
+            HDC hdc = GetDC(hwnd);
+            for (int i = 0; i < arr->count; ++i) {
+                SetPixel(hdc, arr->pixels[i].x, arr->pixels[i].y, arr->pixels[i].color);
             }
-
-            EndPaint(hwnd, &ps);
+            ReleaseDC(hwnd, hdc);
+            delete[] arr->pixels;
+            delete arr;
             return 0;
         }
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
 
-void init_display_buffer() {
-    for (int x = 0; x < HEIGHT; x++) {
-        for (int y = 0; y < WIDTH; y++) {
-            for (int z = 0; z < 6; z++) {
-                display_buffer[x][y][z] = 0;
-            }
-        }
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 }
 
-void synchronize_layers() {
-    for (int x = 0; x < HEIGHT; x++) {
-        for (int y = 0; y < WIDTH; y++) {
-            for (int z = 1; z < 6; z++) {
-                if(display_buffer[x][y][z] == 0) {
-                    display_buffer[x][y][z] = -abs(display_buffer[x][y][z - 1]);
-                }
-            }
-        }
-    }
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // AllocConsole();
-    // freopen("CONOUT$", "w", stdout);
-    // freopen("CONOUT$", "w", stderr);
-
-    int windowWidth = WIDTH + 100;
-    int windowHeight = HEIGHT + 100;
-
-    //this will not go here, here we will process just the request queue
-    //every layer will have separate thread and will be processed in parallel
-    //they all will request pixel update into the queue, and we will process it in this loop
-
-    //initialize the display buffer
-    {
-        init_display_buffer();
-
-        init_bg();
-        init_graphing();
-        init_axis();
-        init_axis_annotations();
-        init_wave_reading();
-        init_annotations();
-
-        synchronize_layers();
-    }
-
-    //start the parallel threads for every layer
-    {
-        std::thread backgroundThread(continue_bg);
-        backgroundThread.detach();
-
-        std::thread graphingThread(continue_graphing);
-        graphingThread.detach();
-
-        std::thread axisThread(continue_axis);
-        axisThread.detach();
-
-        std::thread axisAnnotationsThread(continue_axis_annotations);
-        axisAnnotationsThread.detach();
-
-        std::thread waveReadingThread(continue_wave_reading);
-        waveReadingThread.detach();
-    }
-
-    const char CLASS_NAME[] = "Sample Window Class";
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+    const char CLASS_NAME[] = "PixelWindow";
 
     WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
-
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindowEx(
+    g_hwnd = CreateWindowEx(
         0,
         CLASS_NAME,
-        "Sample Window",
+        "Dynamic Pixel Updater",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, windowWidth, windowHeight,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
+        CW_USEDEFAULT, CW_USEDEFAULT, WIDTH, HEIGHT,
+        NULL, NULL, hInstance, NULL
     );
 
-    if (hwnd == NULL) {
-        return 0;
-    }
+    if (!g_hwnd) return 0;
+    ShowWindow(g_hwnd, nCmdShow);
 
-    ShowWindow(hwnd, nCmdShow);
+    // 🔁 Example usage of dynamic pixel update
+    std::thread([]() {
+        for (int y = 0; y < HEIGHT; ++y) {
+            std::vector<PixelUpdate> row;
+            for (int x = 0; x < WIDTH; ++x) {
+                row.push_back({ x, y, RGB(x % 256, y % 256, (x + y) % 256) });
+            }
+            updatePixels(row); // send one row at a time
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+    }).detach();
 
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
--- firmwire_cpp_up/ui_elements/ui_elem.h
+++
@@ -1,7 +0,0 @@
-#ifndef UI_ELEM_H
-#define UI_ELEM_H
-
-void print_outline();
-
-#endif
-
